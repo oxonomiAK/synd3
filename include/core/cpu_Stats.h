8e43a953f1ea8ea37278f80aa9a71bbb500dd27e
oxonomii <grejs1704@gmail.com>
2025-05-28 19:10:04 +0300

    modified cpu_Stats.h

--- include/core/cpu_Stats.h
+++ include/core/cpu_Stats.h
@@ -19,17 +19,16 @@ extern "C"
 
     int get_cpu_stats(CpuStats *out); // neutral func
     void getProcStat(char *buffer, const int size);
+    void calcTotalCpuUsage(char *prevBuffer, char *currentBuffer);
     void calcEveryCoreUsage(char *prevBuffer, char *currentBuffer);
+    void getTotalCpuUsage();
     void getCpuStats(float *total_usage, float *per_core, size_t size1, size_t size2);
     void CoreCount();
     void getCoreCount(size_t *coreCount, size_t coreCountSize);
     void CpuName();
-    void getThreadCount(size_t *threadCount, size_t threadCountSize);
+    void getCpuName(char *cpuName, size_t cpuNameSize);
     void ThreadCount();
-    void getProcStat(char *buffer, const int size);
-    void calcTotalCpuUsage(char *prevBuffer, char *currentBuffer);
-    void getTotalCpuUsage();
-    unsigned long long getTotalCpuTck(char *prevBuffer, char *currentBuffer);
+    void getThreadCount(size_t *threadCount, size_t threadCountSize);
     unsigned long long calcTotalCpuTck(char *prevBuffer, char *currentBuffer);
 
 #ifdef __cplusplus
